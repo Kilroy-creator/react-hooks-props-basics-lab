82422e60e10e17ff1eb96e6299792982357a578a
Kilroy-creator <royhenry877@gmail.com>
2025-10-21 11:45:04 +0300

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
+  console.log(user);
+
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      {/* 👇 The test is looking for this <nav> tag */}
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+
+      {/* 👇 The test also checks these IDs */}
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
